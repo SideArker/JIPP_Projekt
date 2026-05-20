b3cac93fd2e21f8f282811138a7b92f46dd016a6
SideArker <rsowa126@gmail.com>
2026-05-20 23:34:34 +0200

    tile upgrade

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -6,24 +6,36 @@
 
 int main() {
     sf::RenderWindow window(sf::VideoMode({ 1280, 720 }), "Map Renderer", sf::State::Windowed);
-    sf::View view(sf::FloatRect({ 0.f, 0.f }, { 320.f, 180.f }));
+    sf::View view(sf::FloatRect({ 0.f, 0.f }, { 800.f, 500.f }));
     window.setView(view);
 
     std::vector<Tile> level = {
-        {1, true}, {2, true}, {1, true}, {2, true}, {5, false}, {1, true},
-        {1, true}, {1, true}, {5, false}, {1, true}, {1, true}, {1, true},
-        {1, true}, {5, false}, {1, true}, {1, true}, {1, true}, {1, true},
-        {1, true}, {2, true}, {1, true}, {1, true}, {5, false}, {1, true}
+        // Row 0: Open water
+        {7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},
+        {7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},
+        {7, false},{7, false},{7, false},{7, false},{7, false},{5, false},{5, false},{5, false},{5, false},{5, false},{5, false},{7, false},{7, false},{7, false},{7, false},{7, false},
+        {7, false},{7, false},{7, false},{7, false},{5, false},{1, true}, {1, true}, {2, true}, {1, true}, {1, true}, {1, true}, {6, false},{7, false},{7, false},{7, false},{7, false},
+        {7, false},{7, false},{7, false},{7, false},{1, true}, {1, true}, {1, true}, {1, true}, {3, false},{1, true}, {1, true}, {6, false},{7, false},{7, false},{7, false},{7, false},
+        {7, false},{7, false},{7, false},{1, true}, {1, true}, {1, true}, {2, true}, {1, true}, {1, true}, {1, true}, {1, true}, {6, false},{7, false},{7, false},{7, false},{7, false},
+        {7, false},{7, false},{7, false},{1, true}, {1, true}, {3, false},{1, true}, {1, true}, {1, true}, {1, true}, {1, true}, {1, true}, {6, false},{7, false},{7, false},{7, false},
+        {7, false},{7, false},{7, false},{1, true}, {1, true}, {1, true}, {1, true}, {2, true}, {1, true}, {1, true}, {1, true}, {1, true}, {6, false},{7, false},{7, false},{7, false},
+        {7, false},{7, false},{7, false},{1, true}, {2, true}, {1, true}, {1, true}, {1, true}, {1, true}, {3, false},{1, true}, {6, false},{7, false},{7, false},{7, false},{7, false},
+        {7, false},{7, false},{7, false},{7, false},{1, true}, {1, true}, {1, true}, {1, true}, {1, true}, {1, true}, {1, true}, {6, false},{7, false},{7, false},{7, false},{7, false},
+        {7, false},{7, false},{7, false},{7, false},{7, false},{1, true}, {1, true}, {2, true}, {1, true}, {1, true}, {6, false},{7, false},{7, false},{7, false},{7, false},{7, false},
+        {7, false},{7, false},{7, false},{7, false},{7, false},{4, false},{4, false},{4, false},{4, false},{4, false},{4, false},{7, false},{7, false},{7, false},{7, false},{7, false},
+        {7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},
+        {7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},
+        {7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},
+        {7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false},{7, false}
     };
-
     MapManager mapManager;
 
-    if (!mapManager.loadMap("Art/tileset.png", sf::Vector2u(32, 32), level, 6, 4)) {
+    if (!mapManager.loadMap("Art/tileset.png", sf::Vector2u(32, 32), level, 16, 16)) {
         return -1;
     }
 
     auto player = std::make_shared<Unit>("Tank", "Art/tank-Shoot.png", 20, 5, 5);
-    mapManager.spawnUnit(player, 1, 1);
+    mapManager.spawnUnit(player, 0,0);
     mapManager.setupInput(window);
 
     sf::Clock clock;
